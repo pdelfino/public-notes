1aa6d110223a2a4d65c3e016152b42624c35c5f0
Pedro Delfino <p.delfino01@gmail.com>
2021-05-31 13:22:40 -0300

    Update auto-config.lisp

--- auto-config.lisp
+++ auto-config.lisp
@@ -13,9 +13,3 @@
 (DEFINE-CONFIGURATION (BUFFER WEB-BUFFER)
   ((DEFAULT-MODES %SLOT-DEFAULT%)))
   
-(define-bookmarklet-command hack-youtube-speed 
-   "Change youtube videos speed beyond 3x" 
-   "(function() { const rate = prompt('Set the new playback rate', 2.5); 
-        if (rate != null) {
-            const video = document.getElementsByTagName('video')[0]; 
-            video.playbackRate = parseFloat(rate); } })();")
