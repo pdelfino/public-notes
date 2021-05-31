6c123998824914918a1fd0786b5225ca13d47d93
Pedro Delfino <p.delfino01@gmail.com>
2021-05-31 13:23:07 -0300

    Update init.lisp

--- init.lisp
+++ init.lisp
@@ -1,1 +1,7 @@
 
+(define-bookmarklet-command hack-youtube-speed 
+   "Change youtube videos speed beyond 3x" 
+   "(function() { const rate = prompt('Set the new playback rate', 2.5); 
+        if (rate != null) {
+            const video = document.getElementsByTagName('video')[0]; 
+            video.playbackRate = parseFloat(rate); } })();")
